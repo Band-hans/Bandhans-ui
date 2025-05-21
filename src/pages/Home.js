b197ec12bacd1048620973ec6fbaaf778b07f6e9
Band-hans <info@bandhans.com>
2025-05-21 16:33:50 +1000

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <main>
+      <h2>Home Page</h2>
+      <p>This is the Bandhans frontend app.</p>
+    </main>
+  );
+}
+
+export default Home;
